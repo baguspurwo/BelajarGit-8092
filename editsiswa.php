cfc6408278820f87cd27faec2d389b257937b186
baguspurwo <bagus8092@students.amikom.ac.id>
2016-12-06 14:12:54 +0700

    setup awal project

---
+++ editsiswa.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once('lib/DBClass.php');
+require_once('lib/m_siswa.php');
+require_once('lib/m_nationality.php');
+
+if(!isset($_POST['kirim'])){
+	exit('access forbiden');
+}
+
+$siswa = new Siswa();
+
+if(!copy($_FILES['in_foto']['tmp_name'], 'img/' .$_POST['in_nis'].'.png')){
+	exit('Error Upload File');
+}
+
+	$data['input_name'] = $_POST['in_nama'];
+	$data['input_email'] = $_POST['in_email'];
+	$data['input_nationality'] = $_POST['in_nation'];
+	$data['foto'] = 'img/' .$_POST['in_nis'].'.png';
+
+	$siswa->updateSiswa($_POST['in_nis'], $data);
+
+
+echo "data siswa berhasil di update <br />";
+echo "<a href = 'siswa.php?id=".$_POST['in_nis']."'>Detail Siswa</a>"
+?> 
---
+++ tsiswa.php
@@ -0,0 +1,43 @@
+<?php
+require_once('lib/DBClass.php');
+require_once('lib/m_siswa.php');
+require_once('lib/m_nationality.php');
+
+$siswa = new siswa();
+$nation = new nationality();
+
+$data_nation = $nation->readAllNationality();
+if(isset($_POST['kirim'])){
+
+
+	$name =$_POST['in_nama'];
+	$nis =$_POST['in_nis'];
+	$email = $_POST ['in_email'];
+	$nat = $_POST ['in_nation'];
+
+	$tambah = $siswa->createSiswa($nat, $nis, $name, $email,'');
+	echo "data siswa berhasil ditambah <br /><br />";
+}
+
+?>
+
+<h1> Tambah data siswa </h1>
+<form action="tsiswa.php" method="POST">
+NIS:<br />
+<input type="text" name="in_nis"><br />
+Nama Lengkap:<br />
+<input type="text" name="in_nama"><br />
+Email:<br />
+<input type="text" name="in_email"><br />
+Kewarganegaraan <br />
+<select name="in_nation">
+	<option value="">--pilih negara--</option>
+	<?php foreach($data_nation as $n): ?>
+	<option value="<?php echo $n['id_nationality'] ?>">
+	<?php echo $n['nationality'] ?>	
+	</option>
+	<?php endforeach?> 
+</select> 
+<br />
+<input type="submit" name="kirim" value="simpan">
+</form>
